6d5d333ce83f2c7ada1a727b82e815f48cc3ef90
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-05-16 15:39:57 +0300

    fix: change color for dark theme

--- minesweeper/components/index.js
+++ minesweeper/components/index.js
@@ -1,2 +1,12 @@
 // Styles
 // import '../pages/index.css';
+
+const body = document.querySelector('.body');
+const theme = document.querySelectorAll('.form__theme');
+
+theme.forEach(radio => {
+  radio.addEventListener('change', (evt) => {
+    body.classList.toggle('theme_dark');
+    body.classList.toggle('theme_light');
+  });
+})
